1455eb21cf9cf373a6ae0357a7e76e30193124db
Tyler Hardy <tylerthardy@gmail.com>
2022-01-09 14:13:24 -0500

    Fix issue w merge & formatting

--- src/main/java/com/tylerthardy/taskstracker/tasktypes/league3/League3Task.java
+++ src/main/java/com/tylerthardy/taskstracker/tasktypes/league3/League3Task.java
@@ -5,20 +5,22 @@ import com.tylerthardy.taskstracker.tasktypes.RequiredSkill;
 import com.tylerthardy.taskstracker.tasktypes.Task;
 import com.tylerthardy.taskstracker.tasktypes.TaskPanel;
 import net.runelite.client.callback.ClientThread;
+import net.runelite.client.game.SkillIconManager;
 import net.runelite.client.game.SpriteManager;
 
 public class League3Task extends Task
 {
-    public RequiredSkill[] skills;
-    public String other;
+	public RequiredSkill[] skills;
+	public String other;
 
-    public League3Task(String name, String description, String tier)
+	public League3Task(String name, String description, String tier)
     {
         super(name, description, tier);
     }
 
     @Override
-    public TaskPanel generatePanel(TasksTrackerPlugin plugin, ClientThread clientThread, SpriteManager spriteManager) {
-        return new League3TaskPanel(plugin, clientThread, spriteManager, this);
-    }
+	public TaskPanel generatePanel(TasksTrackerPlugin plugin, ClientThread clientThread, SpriteManager spriteManager, SkillIconManager skillIconManager)
+	{
+		return new League3TaskPanel(plugin, clientThread, spriteManager, this);
+	}
 }
